9564202c6ef63c5886ac2b781d2d4b8471086226
ming <han.568@osu.edu>
2014-10-09 12:34:48 -0400

    added sample code

---
+++ xml/src/TrackingDataXmlWriter.java
@@ -0,0 +1,28 @@
+/**
+ * Created by dayouxia on 10/9/14.
+ */
+public class TrackingDataXmlWriter {
+
+
+    public static void writeSensorsElement(String locationToSaveTheFile){
+
+        // you need to write the code to write everything under <Sensors> tag, it is ok to hardcode it, but do put comments
+        // name ths output file Sensors.xml
+    }
+
+    public static void writeConnectionsElement(String locationToSaveTheFile){
+
+        // you need to write the code to write everything under <Connections> tag, it is ok to hardcode it, but do put comments
+        // name this output file Connections.xml
+    }
+
+
+    public static void main(String[] argv[]){
+        System.out.print("hello");
+        // test if it works here
+        // uncomment and substitute the path
+//        writeConnectionsElement("***substitute this with the directory on your computer***");
+//        writeSensorsElement("***substitute this with the directory on your computer***");
+
+    }
+}
